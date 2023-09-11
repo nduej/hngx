eaad53dda8523e0f7dc629430693f9d67b049f67
Michael Ndue <95193983+nduej@users.noreply.github.com>
2023-09-11 21:36:12 +0100

    Add files via upload

---
+++ Api/index.php
@@ -0,0 +1,32 @@
+<?php
+// Ensure that PHP returns JSON responses
+date_default_timezone_set('UTC');
+header('Content-Type: application/json');
+
+// Function to validate UTC time within +/-2
+// Check if 'slack_name' and 'utc_time' parameters are present in the query string
+if (isset($_GET['slack_name']) && isset($_GET['track'])) {
+    $slackName = $_GET['slack_name'];
+    $track = $_GET['track'];
+    // Get the GitHub URLs (replace with your actual URLs)
+    $githubFileURL = 'https://github.com/14326a/hndrepo/blob/main/api.php';
+    $githubSourceURL = 'https://github.com/nduej/hngx.git';
+    // Set the timezone to UTC
+    $utcDateTime = gmdate('Y-m-d\TH:i:s\Z');
+    $currentDay = date('l');
+    $response = [
+        'slack_name' => $slackName,
+        'day_of_week' => $currentDay,
+        'utc_time' => $utcDateTime,
+        'track' => $track,
+        'github_file_url' => $githubFileURL,
+        'github_source_url' => $githubSourceURL,
+        'status_code' => 200,
+    ];
+
+    echo json_encode($response);
+} else {
+
+    echo json_encode(['error' => 'Missing required parameters']);
+    http_response_code(400);
+}
